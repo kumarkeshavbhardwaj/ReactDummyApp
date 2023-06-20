5d923df513225f19a3700189d1fa79c05b1e1f87
kumarkeshavbhardwaj <kumarkeshavbhardwaj@gmail.com>
2023-06-20 21:06:59 +0530

    Stying and Separate Component for Date

--- dummyapp/src/components/ExpenseItem.js
+++ dummyapp/src/components/ExpenseItem.js
@@ -1,23 +1,12 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem (props) {
-
-    const month = props.date.toLocaleString('en-US', {month: 'long'});
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-    const year = props.date.getFullYear();
-
-    // const expenseDate = new Date(2021, 2, 28);
-    // const expenseTitle = 'Car Insurance';
-    // const expenseAmount = 234.50;
 
 
     return (
     <div className="expense-item">
-        <div>
-            <div>{month}</div>
-            <div>{year}</div>
-            <div>{day}</div>
-        </div>
+        <ExpenseDate date={props.date} />
         <div className="expense-item__description">
             <h2>{props.title}</h2>
             <div className="expense-item__price">${props.amount}</div>
